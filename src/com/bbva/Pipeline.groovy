ca279a48c9d41cd852126253430d601a4ed88c56
e049773 <test@test.com>
2019-11-18 16:47:57 +0100

    851

--- src/com/bbva/Pipeline.groovy
+++ src/com/bbva/Pipeline.groovy
@@ -14,7 +14,7 @@ public class Pipeline {
 
     def Pipeline(){
         print ("pipelineId: " +pipelineId)
-        pipelineId = 'test_pipeline')
+        pipelineId = 'test_pipeline'
         print ("pipelineId: " +pipelineId)
     }
     
